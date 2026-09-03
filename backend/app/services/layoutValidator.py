d7bb83883b511417fb28553828fd4669f9941fde
OxOv3rH4uL <oterinaree@gmail.com>
2026-09-03 20:10:39 +0200

    Feat: AI Service and Validation Changes

--- backend/app/services/layoutValidator.py
+++ backend/app/services/layoutValidator.py
@@ -6,6 +6,11 @@ class LayoutValidator:
         errors.extend(self.overlap_check(plan))
         errors.extend(self.dimension_check(plan))
         errors.extend(self.floor_boundary_check(plan))
+        errors.extend(self.room_within_floor_boundary_check(plan))
+        errors.extend(self.total_floor_check(plan))
+        errors.extend(self.floor_number_check(plan))
+        
+
         return errors
 
     def overlap_check(self,plan: HousePlan) -> list[str]:
@@ -49,3 +54,41 @@ class LayoutValidator:
                 errors.append(f"Floor {floor_plan.floor} has invalid height boundary")
 
         return errors
+
+    def room_within_floor_boundary_check(self,plan: HousePlan) -> list[str]:
+        errors = []
+
+        for floor_plan in plan.floor_plan:
+            boundary = floor_plan.boundary
+            for room in floor_plan.rooms:
+                if(room.x < 0):
+                    errors.append(f"{room.name} goes beyond the left boundary of the floor {floor_plan.floor}")
+                if(room.y < 0):
+                    errors.append(f"{room.name} goes beyond the top boundary of the floor {floor_plan.floor}")
+
+                if(room.x + room.width > boundary.width):
+                    errors.append(f"{room.name} width exceeds the floor {floor_plan.floor} width boundary")
+
+                if(room.y + room.height > boundary.height):
+                    errors.append(f"{room.name} height exceeds the floor {floor_plan.floor} height boundary")
+
+        return errors
+
+    def total_floor_check(self,plan : HousePlan) -> list[str]:
+        errors = []
+        if(len(plan.floor_plan) != plan.floors):
+            errors.append(f"Expected {plan.floors} floor plans but received only {len(plan.floor_plan)} floor plans")
+
+        return errors
+
+    def floor_number_check(self, plan : HousePlan) -> list[str]:
+        errors = []
+
+        for floor_plan in plan.floor_plan:
+            if(floor_plan.floor <= 0):
+                errors.append(f"Invalid Floor Number {floor_plan.floor}")
+            if(floor_plan.floor > plan.floors):
+                errors.append(f"Floor {floor_plan.floor} exceeeds total number of floors {plan.floors}")
+
+        return errors
+    
